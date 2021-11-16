50b21afef2b542bfe080161c8bcfab15ba4f3fdc
Your Name <nickk.lima17@gmail.com>
2021-11-16 14:29:21 -0300

    Att imovel

--- src/PáginaPrincipal.tsx
+++ src/PáginaPrincipal.tsx
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import Imovel from './backend/DTO/dtos';
 import fetch from 'node-fetch';
+import { Button, ButtonGroup, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function PaginaPrincipal() {
     const [dados, setDados] = useState<Array<Imovel>>();
-    // const [url, setUrl] = useState(process.env.URL!);
+    const [url, setUrl] = useState(process.env.URL!);
     const [carregando, setCarregando] = useState(false);
     const [erro, setErro] = useState(false);
+    const [elements, setElements] = useState<Array<JSX.Element>>([]);
 
     useEffect(() => {
         async function consultarWebServer() {
             setErro(false);
-            setCarregando(true);
-
             try {
-                const resultado = await fetch('https://ws-airbnbclone-1226.herokuapp.com/');
+                setCarregando(false);
+                const resultado = await fetch('http://localhost:5000');
                 if (resultado.ok) {
                     const dados: Array<Imovel> = await resultado.json();
                     setDados(dados);
@@ -29,7 +31,33 @@ export default function PaginaPrincipal() {
         }
 
         consultarWebServer();
-    });
+    }, [url]);
+
+    useEffect(() => {
+        function Elements(dados: Array<Imovel>) {
+            let element = [];
+            for (let i of dados) {
+                element.push(
+                    <>
+                        <Container>
+                            <Button variant="light">
+                                <p>idCasa: {i.iId}</p>
+                                <p>label: {i.label}</p>
+                                <p>espaco: {i.espaco}</p>
+                                <p>Maximo de Hóspedes: {i.nHospedes}</p>
+                                <p>Quartos: {i.nQuartos}</p>
+                            </Button>
+                        </Container>
+
+                    </>
+                );
+            }
+
+            setElements(element);
+        }
+
+        Elements(dados || []);
+    }, [dados]);
 
     return (
         <>
@@ -38,14 +66,7 @@ export default function PaginaPrincipal() {
             {carregando ? (
                 <div>Carregando...</div>
             ) : (
-                dados && (
-                    dados.forEach((element) => {
-                        <div>
-                            <p>espaco: {element.espaco} </p>
-                            <p>label: {element.label}</p>
-                        </div>
-                    })
-                )
+                dados && (<div> <ButtonGroup vertical> {elements} </ButtonGroup></div>)
             )}
         </>
     );
